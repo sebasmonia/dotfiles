41241e23e5c4579b92918760aa9007437350d026
Sebastián Monía <smonia@outlook.com>
2019-12-20 15:32:39 -0700

    Add xsetwacom configuration (right click + middle click)

---
+++ wacomconfig.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Top button is right click
+xsetwacom set "Wacom Pen and multitouch sensor Pen stylus" Button 2 "3"
+
+# Bottom button, pressed when touching the screen is middle click
+xsetwacom set "Wacom Pen and multitouch sensor Pen eraser" Button 1 "2"
