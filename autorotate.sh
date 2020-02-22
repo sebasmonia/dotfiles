9154833bdc94c753126f8f73419e91dd0c0bf54f
Sebastián Monía <smonia@outlook.com>
2020-02-22 15:42:23 -0700

    Modify autorotate.sh using input from https://github.com/linuxwacom/input-wacom/issues/145

--- autorotate.sh
+++ autorotate.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Source: https://gist.github.com/mortie/e725d37a71779b18e8eaaf4f8a02bf5b
-# via Reddit
+# (via Reddit)
+# 2020-02-22 After reporting a bug in xsetwacom, which wasn't really their
+# problem, updated the script to use xsetwacom and no matrix, see
+# https://github.com/linuxwacom/xf86-input-wacom/wiki/Rotation
 
 # Automatically rotate the screen when the device's orientation changes.
 
@@ -10,24 +13,25 @@ monitor-sensor \
 	| while read -r line; do
 		line="${line#??}"
 		if [ "$line" = "normal" ]; then
-			rotate=normal
-			matrix="0 0 0 0 0 0 0 0 0"
+			screenrotate=normal
+                        wacomrotate=none
 		elif [ "$line" = "left-up" ]; then
-			rotate=left
-			matrix="0 -1 1 1 0 0 0 0 1"
+			screenrotate=left
+                        wacomrotate=ccw
 		elif [ "$line" = "right-up" ]; then
-			rotate=right
-			matrix="0 1 0 -1 0 1 0 0 1"
+			screenrotate=right
+                        wacomrotate=cw
 		elif [ "$line" = "bottom-up" ]; then
-			rotate=inverted
-			matrix="-1 0 1 0 -1 1 0 0 1"
+			screenrotate=inverted
+                        wacomrotate=half
 		else
 			echo "Unknown rotation: $line"
 			continue
 		fi
 
-		xrandr --output "eDP1" --rotate "$rotate"
-		xinput set-prop "Wacom Pen and multitouch sensor Finger touch" --type=float "Coordinate Transformation Matrix" $matrix
-		xinput set-prop "Wacom Pen and multitouch sensor Pen stylus" --type=float "Coordinate Transformation Matrix" $matrix
-		xinput set-prop "Wacom Pen and multitouch sensor Pen eraser" --type=float "Coordinate Transformation Matrix" $matrix
+		xrandr --output "eDP1" --rotate "$screenrotate"
+                xsetwacom set "Wacom Pen and multitouch sensor Finger touch" rotate $wacomrotate
+                xsetwacom set "Wacom Pen and multitouch sensor Pen stylus" rotate $wacomrotate
+                xsetwacom set "Wacom Pen and multitouch sensor Pen eraser" rotate $wacomrotate
+                nitrogen --restore
 	done
