faa285344fb61fa529ea94d37cab5ac6c298f737
Sebastián Monía <smonia@outlook.com>
2020-03-22 22:50:59 -0600

    Use bottom pen button to toggle CellWriter

--- wacomconfig.sh
+++ wacomconfig.sh
@@ -6,5 +6,8 @@ xsetwacom set "Wacom Pen and multitouch sensor Pen stylus" Button 2 "3"
 
 # Bottom button, pressed when touching the screen is middle click
 # Default value: button +1 (same as regular click)
-xsetwacom set "Wacom Pen and multitouch sensor Pen eraser" Button 1 "2"
-# xsetwacom set "Wacom Pen and multitouch sensor Pen eraser" Button 1 pan
+# xsetwacom set "Wacom Pen and multitouch sensor Pen eraser" Button 1 "2"
+
+# Experimental: Bottom button, pressed when touching the screen brings up CellWriter
+# Uses xbindkeys to run a script on mouse button 11
+xsetwacom set "Wacom Pen and multitouch sensor Pen eraser" Button 1 "11"
