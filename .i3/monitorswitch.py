7ea4b308c67f7ff7c38df5e3d980d875c4d0cd03
Sebastián Monía <smonia@outlook.com>
2020-07-12 19:23:16 -0600

    Update monitorswitch.py to support DP2 OR HDMI2 (only one output at a time)

--- .i3/monitorswitch.py
+++ .i3/monitorswitch.py
@@ -6,32 +6,53 @@ import subprocess
 import sys
 
 
+def xrandr(params=None):
+    p = ["xrandr"]
+    if params:
+        p.extend(str(e) for e in params)
+    result = subprocess.run(p,
+                            stdout=subprocess.PIPE)
+    return result.stdout.decode('utf-8')
+
+
+def get_outputs():
+    data = xrandr().splitlines()
+    return ([x.split()[0] for x in data if " connected" in x],
+            [x.split()[0] for x in data if " disconnected" in x])
+
+
 def get_arg_or_exit():
     global xrandr_params
     if len(sys.argv) < 2:
         sys.exit(1)
     arg = sys.argv[1]
-    if arg not in xrandr_params:
+    if arg not in xrandr_configs:
         sys.exit(1)
     return arg
 
 
-xrandr_params = {"internal": ["--output", "HDMI2", "--off",
-                              "--output", "eDP1", "--auto", "--primary"],
-                 "external": ["--output", "HDMI2", "--auto", "--primary",
-                              "--output", "eDP1", "--off"],
-                 "both-right": ["--output", "eDP1", "--auto", "--primary",
-                                "--output", "HDMI2", "--auto", "--right-of",
-                                "eDP1"],
-                 "both-left": ["--output", "eDP1", "--auto", "--primary",
-                               "--output", "HDMI2", "--auto", "--left-of",
-                               "eDP1"]}
+xrandr_configs = {"internal": ["--output", "HDMI2", "--off",
+                               "--output", "DP2", "--off",
+                               "--output", "eDP1", "--auto", "--primary"],
+                  "external": ["--output", "{external}", "--auto", "--primary",
+                               "--output", "eDP1", "--off"],
+                  "both-right": ["--output", "eDP1", "--auto", "--primary",
+                                 "--output", "{external}", "--auto", "--right-of",
+                                 "eDP1"],
+                  "both-left": ["--output", "eDP1", "--auto", "--primary",
+                                "--output", "{external}", "--auto", "--left-of",
+                                "eDP1"]}
 
 arg = get_arg_or_exit()
 
-params = ["xrandr"]
-params.extend(xrandr_params[arg])
-subprocess.run(params)
+# This script is meant for ONE external output at a time
+connected, disconnected = get_outputs()
+external_output = "DP2" if "DP2" in connected else "HDMI2"
+
+# If the config selected has a marker for {external}, replace it
+params = [external_output if x == "{external}" else x
+          for x in xrandr_configs[arg]]
+xrandr(params)
 
 subprocess.run(["killall", "conky"])
 subprocess.run(["nitrogen", "--restore"])
