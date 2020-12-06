1a2902176b7415618e5bd9ff064dd44527781a41
Sebastián Monía <sebastian.monia@starz.com>
2020-12-05 22:48:49 -0700

    Add early-init.el to repository

---
+++ .emacs/early-init.el
@@ -0,0 +1,10 @@
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(tooltip-mode 0)
+
+;; Ignore .Xresources
+(advice-add #'x-apply-session-resources :override #'ignore)
+
+(setq frame-title-format "%b - Emacs"
+      icon-title-format "%b - Emacs")
