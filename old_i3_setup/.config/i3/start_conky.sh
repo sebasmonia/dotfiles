3ab927c8423284e4f68600c49bb2c5a481e5e41d
Sebastián Monía <smonia@outlook.com>
2022-01-05 21:32:21 -0700

    Delete old setup, I don't think I'll leave Fedora Silverblue anytime soon

--- old_i3_setup/.config/i3/start_conky.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-conky -c /home/hoagie/.config/i3/conky_shortcuts &&
-conky -c /home/hoagie/.config/i3/conky_sys &&
-
-exit 0
--- old_i3_setup/.config/i3/trackpointconfig.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-xinput --set-prop "Elan TrackPoint" "libinput Accel Speed" 0.40
-xinput --set-prop "Elan TrackPoint" "libinput Accel Profile Enabled" 0 1
