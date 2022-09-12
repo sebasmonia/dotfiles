2e0480f75649df5e150b4accb3e01a61f03818c7
Sebastián Monía <smonia@outlook.com>
2022-09-11 18:33:31 -0600

    No need to add NonGNU to the package archives

--- .emacs/init.el
+++ .emacs/init.el
@@ -31,8 +31,6 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
-
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
