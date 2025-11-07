d7995e7b18bc69425230c8150c2a7e416690dd39
Sebastián Monía <sebastian@sebasmonia.com>
2025-11-07 17:06:34 -0500

    Add machine-config.el to the repo
    Right before ignoring it...

---
+++ .config/emacs/machine-config.el
@@ -0,0 +1,19 @@
+;;; machine-config.el --- Local overloads -*- lexical-binding: t; -*-
+;; Author: Sebastián Monía <sebastian@sebasmonia.com>
+;; URL: https://git.sr.ht/~sebasmonia/dotfiles
+;; Version: 1.0
+;; Keywords: local
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; This file is in .gitignore
+;; I drop here things that I need to setup per-computer. I used to do it by OS
+;; (Windows = work, Linux = home) but nowadays I also use Linux at work for
+;; some tasks.
+;; I was going to use environment variables, but I think might be simpler.
+;;; Code:
+
+
+;;; machine-config.el ends here
